3fcdc2abb1c7bbcc2824b37294b26cda50bb0417
오상훈 <oshosh12@gmail.com>
2021-09-02 00:23:22 +0900

    useState

---
+++ nemo_usestate/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Nemo from "./Nemo.js"
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <Nemo/>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
