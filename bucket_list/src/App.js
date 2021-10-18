49b501190c52bb0da140cf57ad786899b6b885d4
Sanghun OH <oshosh12@gmail.com>
2021-10-18 18:19:14 +0900

    Update App.js

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -8,18 +8,18 @@ import styled from "styled-components";
 import Detail from "./Detail";
 import NotFound from "./NotFound";
 
-// 리덕스 스토어와 연결하기 위해 connect라는 친구를 호출할게요!
+// 리덕스 스토어와 연결하기 위해 connect라는 친구를 호출
 import {connect} from 'react-redux';
-// 리덕스 모듈에서 (bucket 모듈에서) 액션 생성 함수 두개를 가져올게요!
+// 리덕스 모듈에서 (bucket 모듈에서) 액션 생성 함수 두개를 가져옴
 import {loadBucket, createBucket} from './redux/modules/bucket';
 
-// 이 함수는 스토어가 가진 상태값을 props로 받아오기 위한 함수예요.
+// 이 함수는 스토어가 가진 상태값을 props로 받아오기 위한 함수.
 const mapStateTopProps = (state) => ({
   bucket_list: state.bucket.list,
 });
 
 
-// 이 함수는 값을 변화시키기 위한 액션 생성 함수를 props로 받아오기 위한 함수예요.
+// 이 함수는 값을 변화시키기 위한 액션 생성 함수를 props로 받아오기 위한 함수.
 const mapDispatchToProps = (dispatch) => ({
   load: () => {
     dispatch(loadBucket());
@@ -30,23 +30,17 @@ const mapDispatchToProps = (dispatch) => ({
   }
 });
 
-// 클래스형 컴포넌트는 이렇게 생겼습니다!
+// 클래스형 컴포넌트는 이렇게 생겼다!
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // App 컴포넌트의 state를 정의해줍니다.
+    // App 컴포넌트의 state를 정의.
     this.state = {
      
-      
-      
-      
-      
-      
-      
-      
-      
+
+  
     };
-    // ref는 이렇게 선언합니다!
+    // ref는 이렇게 선언!
     this.text = React.createRef();
   }
 
@@ -59,23 +53,21 @@ class App extends React.Component {
     this.props.create(new_item);
   };
 
-  // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
+  // 랜더 함수 안에 리액트 엘리먼트를 넣는다
   render() {
     return (
       <div className="App">
         <Container>
           <Title>내 버킷리스트</Title>
           <Line />
-          {/* 컴포넌트를 넣어줍니다. */}
-          {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
-          {/* Route 쓰는 법 2가지를 모두 써봅시다! */}
+        
           <Switch>
             <Route path="/" exact component={BucketList} />
             <Route path="/detail/:index" component={Detail} />
             <Route component={NotFound} />
           </Switch>
         </Container>
-        {/* 인풋박스와 추가하기 버튼을 넣어줬어요. */}
+      
         <Input>
           <input type="text" ref={this.text} />
           <button onClick={this.addBucketList}>추가하기</button>
@@ -117,5 +109,5 @@ const Line = styled.hr`
   border: 1px dotted #ddd;
 `;
 // withRouter 적용
-// connect로 묶어줬습니다!
+// connect
 export default connect(mapStateTopProps, mapDispatchToProps)(withRouter(App));
