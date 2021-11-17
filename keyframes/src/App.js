972974f0fac636c7f02b58c7f1b31e847d32c558
오상훈 <oshosh12@gmail.com>
2021-11-17 20:09:41 +0900

    keyframes

--- keyframes/src/App.js
+++ keyframes/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+// styled와 keyframes를 불러옵니다!
+import styled, {keyframes} from "styled-components";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<Box></Box>
+</div>
+);
 }
-
+// 박스를 먼저 하나 만들어줍니다
+const Box = styled.div`
+width: 100px;
+height: 100px;
+background: green;
+`;
 export default App;
