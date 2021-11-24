3898077739f3a1f5d62dcd7ddf6c12cf02874fc6
Sanghun OH <oshosh12@gmail.com>
2021-11-24 22:02:47 +0900

    Update App.js

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -19,6 +19,7 @@ const mapStateTopProps = (state) => ({
   bucket_list: state.bucket.list
 });
 
+
 // 이 함수는 값을 변화시키기 위한 액션 생성 함수를 props로 받아오기 위한 함수
 const mapDispatchToProps = (dispatch) => ({
   load: () => {
